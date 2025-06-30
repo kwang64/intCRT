9dc4e3444655b42eb12c76eefb5494483a7e05ed
Katrina Wang <kwang64@smith.edu>
2025-06-30 22:37:51 +0800

    added roxygen of initialization of Composite EM algorithm function

--- R/Initialization of the Composite EM Algorithm.R
+++ R/Initialization of the Composite EM Algorithm.R
@@ -1,4 +1,12 @@
-### Initializes the coefficient and baseline hazard terms
+#' Initialize the coefficient and the baseline hazard terms
+#'@param p dimension of the coefficient vector
+#'@param tau list of length S containing tau.s for each stratum
+#'@return A list with two components
+#'\describe{
+#'  \item{beta}{A list of vector 0s with length of \code{p}, representing initialized coefficients }
+#'  \item{lambda}{a list of vectors where each vector has an initial baseline hazard value (1/length of tau.s) } }
+#'@export
+
 em_init <- function(p, tau){
 
   # Setting the coefficient vector to zero and the baseline hazard function to 1/rho.s for each stratum
